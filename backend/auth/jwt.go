5cb971afdb79bc8e27b3845830a706494e31859b
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-08-19 00:04:03 +0200

    used defaultClaims for jwt

--- backend/auth/jwt.go
+++ backend/auth/jwt.go
@@ -1,7 +1,8 @@
 package auth
 
 import (
-	"log"
+	"errors"
+	"fmt"
 	"simvino/config"
 	"time"
 
@@ -13,31 +14,44 @@ var (
 	SecretKey = []byte(config.GetSecret())
 )
 
+type defaultClaims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
 // GenerateToken generates a jwt token and assign a email to it's claims and return it
 func GenerateToken(email string) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	/* Create a map to store our claims */
-	claims := token.Claims.(jwt.MapClaims)
-	/* Set token claims */
-	claims["email"] = email
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-	tokenString, err := token.SignedString(SecretKey)
-	if err != nil {
-		log.Fatal("Error in Generating key")
-		return "", err
+
+	claims := defaultClaims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 1000).Unix(),
+			Issuer:    "simvino",
+		},
 	}
-	return tokenString, nil
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+
+	return token.SignedString(SecretKey)
 }
 
 // ParseToken parses a jwt token and returns the email in it's claims
 func ParseToken(tokenStr string) (string, error) {
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		return SecretKey, nil
-	})
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		email := claims["email"].(string)
-		return email, nil
-	} else {
-		return "", err
+	token, err := jwt.ParseWithClaims(
+		tokenStr,
+		&defaultClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return SecretKey, nil
+		},
+	)
+	if err != nil {
+		return "", fmt.Errorf("val wrong")
 	}
+
+	claims, ok := token.Claims.(*defaultClaims)
+	if !ok {
+		return "", errors.New("couldn't parse claims")
+	}
+
+	return claims.Email, nil
 }
