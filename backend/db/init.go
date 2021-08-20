e3acb39cff87b1e08c726e2326855fd9b866da43
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-08-20 18:47:01 +0200

    can now init db

---
+++ backend/db/init.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"io/ioutil"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Init() {
+	initMySQL()
+	initRedis()
+}
+
+func createUser() {
+
+	query, err := ioutil.ReadFile("db/sql/user.sql")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = Db.Exec(string(query))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createBalance() {
+
+	query, err := ioutil.ReadFile("db/sql/balance.sql")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = Db.Exec(string(query))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
