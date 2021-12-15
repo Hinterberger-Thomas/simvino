73bdb2ec56ebdd3886902f5a6a463567c2baba2d
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-12-15 22:12:48 +0100

    added vitesse

--- backend/db/sql/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user`
-(
- `userID`   int NOT NULL AUTO_INCREMENT,
- `password` varchar(255) NOT NULL,
- `email`    varchar(255) NOT NULL UNIQUE,
-
-PRIMARY KEY (`userID`)
-);
